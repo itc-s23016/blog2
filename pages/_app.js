ee72b532172964d286abe9488d8e557884e1b354
itc-s23016 <s23016@std.it-college.ac.jp>
2024-01-23 07:30:26 +0000

    Lesson 2.6
    AppコンポーネントでLayoutを全ページに入れた。

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import "@/styles/globals.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import Layout from 'components/layout'
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
+export { MyApp }
