8bb7b72357f4e9d01ba16a5b129e11ac80ac7ff7
itc-s23016 <s23016@std.it-college.ac.jp>
2024-01-30 05:28:44 +0000

    3.6

--- components/logo.js
+++ components/logo.js
@@ -1,6 +1,11 @@
 import Link from 'next/link'
+import styles from 'styles/logo.module.css'
 
-const Logo = () => {
-  return <Link href='/'>CUBE</Link>
+const Logo = ({ boxOn = false }) => {
+  return (
+    <Link href='/' className={boxOn ? styles.box : styles.basic}>
+      CUBE
+    </Link>
+  )
 }
 export default Logo
