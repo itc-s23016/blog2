427bd4bc2f61bccf3472ebfdcc5f272a07240039
itc-s23016 <s23016@std.it-college.ac.jp>
2024-01-30 07:05:05 +0000

    Lesson 4.8
    ３つのコンポーネントで２段組みのレイアウトを構成した。

---
+++ components/two-column.js
@@ -0,0 +1,17 @@
+import styles from 'styles/two-column.module.css'
+
+const TwoColumn = ({ children }) => {
+  return <div className={styles.flexContainer}>{children}</div>
+}
+
+const TwoColumnMain = ({ children }) => {
+  return <div className={styles.main}>{children}</div>
+}
+
+const TwoColumnSidebar = ({ children }) => {
+  return <div className={styles.sidebar}>{children}</div>
+}
+
+export { TwoColumnSidebar }
+export { TwoColumnMain }
+export { TwoColumn }
